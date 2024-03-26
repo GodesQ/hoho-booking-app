85e6a2d8680dfdad82d2c95a30ce03c2bc557f18
Garnfil <jamesgarnfil15@gmail.com>
2024-03-26 12:27:36 +0800

    add profile page

---
+++ app/(main)/profile/page.jsx
@@ -0,0 +1,11 @@
+import HeaderPage from '@/app/components/HeaderPage'
+import { Spacer } from '@nextui-org/react'
+import React from 'react'
+
+export default function ProfilePage() {
+  return (
+    <div>
+        <Spacer y={60} />
+    </div>
+  )
+}
