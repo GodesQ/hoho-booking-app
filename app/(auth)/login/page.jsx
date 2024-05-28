8861f710c28272ac28b0739e861f0bdf1e67b0a0
Garnfil <jamesgarnfil15@gmail.com>
2024-05-28 08:29:47 +0800

    Add "Back to Home" button on login page

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -6,137 +6,106 @@ import Logo from "../../../public/hoho-logo.jpg";
 import heroBackground from "../../../public/assets/bg-hero.png";
 import { Button, Image, Input } from "@nextui-org/react";
 import { login } from "@/action";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { API_ENDPOINT } from "@/constant";
-
+import Link from "next/link";
 
 export default function LoginPage() {
-  const headerStyling = {
-    backgroundImage: `url(${heroBackground.src})`,
-  };
+    const headerStyling = {
+        backgroundImage: `url(${heroBackground.src})`,
+    };
 
-  const router = useRouter()
-  
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
+    const router = useRouter();
 
-  const [loginBtnDisabled, setLoginBtnDisabled] = useState(false);
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
 
-  const [errorMessage, setErrorMessage] = useState("");
+    const [loginBtnDisabled, setLoginBtnDisabled] = useState(false);
 
-  const handleLoginSubmit = async () => {
-    try {
-      setLoginBtnDisabled(true);
+    const [errorMessage, setErrorMessage] = useState("");
 
-      const response = await fetch(`https://staging.philippines-hoho.ph/api/v2/login`, {
-        method: "POST", 
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-      });
+    const handleLoginSubmit = async () => {
+        try {
+            setLoginBtnDisabled(true);
 
-      console.log(response);
+            const response = await fetch(`https://staging.philippines-hoho.ph/api/v2/login`, {
+                method: "POST",
+                headers: {
+                    accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(credentials),
+            });
 
-      setLoginBtnDisabled(false);
+            console.log(response);
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
+            setLoginBtnDisabled(false);
 
-      const responseData = await response.json();
-      login(responseData);
-      
-      setErrorMessage("");
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message);
+            }
 
-      router.push('/');
+            const responseData = await response.json();
+            login(responseData);
 
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
-  };
+            setErrorMessage("");
 
-  return (
-    <div className="login-main-container">
-      <div className="login-content-container">
-        <div className="flex justify-center items-center flex-col text-center gap-3">
-          <Image
-          classNames={{
-            wrapper: 'max-w-[100px] w-100',
-            img: 'max-w-[100px] w-100'
-          }}
-            src={Logo.src}
-            alt="Philippines Hop On Hop Off"
-          />
-          <h2 className="text-medium sm:text-large" style={{ lineHeight: "35px" }}>
-            Welcome to Philippine Hop On Hop Off Booking Tour Website
-          </h2>
-          <h3 className="text-sm sm:text-medium">
-            Get Ready to Explore - Login and Dive into the Vibrant Culture of
-            the Philippines!
-          </h3>
+            router.push("/");
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
+    };
+
+    return (
+        <div className="login-main-container">
+            <div className="login-content-container">
+                <div className="flex justify-center items-center flex-col text-center gap-3">
+                    <Image
+                        classNames={{
+                            wrapper: "max-w-[100px] w-100",
+                            img: "max-w-[100px] w-100",
+                        }}
+                        src={Logo.src}
+                        alt="Philippines Hop On Hop Off"
+                    />
+                    <h2 className="text-medium sm:text-large" style={{ lineHeight: "35px" }}>
+                        Welcome to Philippine Hop On Hop Off Booking Tour Website
+                    </h2>
+                    <h3 className="text-sm sm:text-medium">Get Ready to Explore - Login and Dive into the Vibrant Culture of the Philippines!</h3>
+                </div>
+                <div className="login-form">
+                    {errorMessage != "" ? <div className="my-4 bg-primary-50 text-primary rounded py-3 px-4">{errorMessage}</div> : <div></div>}
+                    <form action="#">
+                        <div className="my-5">
+                            <label className="text-small" htmlFor="username">
+                                Username <span className="text-danger">*</span>
+                            </label>
+                            <Input type="text" placeholder="Type your username address..." className="mt-2" onInput={(e) => setCredentials({ ...credentials, username: e.target.value })} />
+                        </div>
+                        <div className="my-5">
+                            <label className="text-small" htmlFor="password">
+                                Password <span className="text-danger">*</span>
+                            </label>
+                            <Input type="password" placeholder="Type your password..." className="mt-2" onInput={(e) => setCredentials({ ...credentials, password: e.target.value })} />
+                        </div>
+                        <Button isDisabled={loginBtnDisabled} className="bg-primary text-foreground w-full rounded-xl shadow" onClick={handleLoginSubmit}>
+                            Sign In
+                        </Button>
+                    </form>
+                    <div className="my-3 flex justify-center">
+                        <Link href="/" className="text-primary text-center">Back to Home</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="login-image-container" style={headerStyling}>
+                <Image src={LogoText.src} width={360} className="nav-logo" alt="Philippines Hop On Hop Off" />
+                <h2 className="text-small">
+                    Explore the Beauty of the Philippines with Ease - Login to Plan Your Adventure! Dive into a World of Stunning Landscapes, Rich Culture, and Unforgettable Experiences.
+                </h2>
+            </div>
         </div>
-        <div className="login-form">
-          {errorMessage != "" ? (
-            <div className="my-4 bg-primary-50 text-primary rounded py-3 px-4">
-              {errorMessage}
-            </div>
-          ) : (
-            <div></div>
-          )}
-          <form action="#">
-            <div className="my-5">
-              <label className="text-small" htmlFor="username">
-                Username <span className="text-danger">*</span>
-              </label>
-              <Input
-                type="text"
-                placeholder="Type your username address..."
-                className="mt-2"
-                onInput={(e) =>
-                  setCredentials({ ...credentials, username: e.target.value })
-                }
-              />
-            </div>
-            <div className="my-5">
-              <label className="text-small" htmlFor="password">
-                Password <span className="text-danger">*</span>
-              </label>
-              <Input
-                type="password"
-                placeholder="Type your password..."
-                className="mt-2"
-                onInput={(e) =>
-                  setCredentials({ ...credentials, password: e.target.value })
-                }
-              />
-            </div>
-            <Button isDisabled={loginBtnDisabled}
-              className="bg-primary text-foreground w-full rounded-xl shadow"
-              onClick={handleLoginSubmit}
-            >
-              Sign In
-            </Button>
-          </form>
-        </div>
-      </div>
-      <div className="login-image-container" style={headerStyling}>
-        <Image
-          src={LogoText.src}
-          width={360}
-          className="nav-logo"
-          alt="Philippines Hop On Hop Off"
-        />
-        <h2 className="text-small">
-          Explore the Beauty of the Philippines with Ease - Login to Plan Your
-          Adventure! Dive into a World of Stunning Landscapes, Rich Culture, and
-          Unforgettable Experiences.
-        </h2>
-      </div>
-    </div>
-  );
+    );
 }
